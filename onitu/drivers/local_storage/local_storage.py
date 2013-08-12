81a648cffa44c7d7008d39620adbda50924e2341
Antoine Rozo <antoine.rozo@gmail.com>
2013-08-12 21:02:04 +0200

    local_storage: read_chunk, write_chunk, new_file

--- onitu/drivers/local_storage/local_storage.py
+++ onitu/drivers/local_storage/local_storage.py
@@ -1,5 +1,7 @@
 from onitu.api.plug import Plug
+from os import path, makedirs
 
+root = './'
 plug = Plug()
 
 @plug.handler
@@ -8,9 +10,23 @@ def send_chunk(chunk):
     return True
 
 @plug.handler
-def read_chunk(chunk):
-    content = "Fill this with the real chunk, bitch !"
-    return content
+def read_chunk(filename, offset, size):
+    with open(filename, 'rb') as f:
+        f.seek(offset)
+        return f.read(size)
+
+@plug.handler
+def write_chunk(filename, offset, chunk):
+    with open(filename, 'rb') as f:
+        f.seek(offset)
+        return f.write(chunk)
+
+@plug.handler
+def new_file(metadatas):
+    filepath = root + metadatas['path']
+    if not path.exists(filepath):
+        makedirs(filepath)
+    open(path.join(filepath, metadatas['filename']), 'w+').close()
 
 def start(*args, **kwargs):
     plug.start()
