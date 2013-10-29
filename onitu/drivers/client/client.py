fc6aec993a111d484dfe5f58f4115a0a26c898d5
Antoine Rozo <antoine.rozo@gmail.com>
2013-10-29 13:52:47 +0100

    Reorder statements

--- onitu/drivers/client/client.py
+++ onitu/drivers/client/client.py
@@ -5,15 +5,15 @@ from threading import Thread
 plug = Plug()
 plug.client = None
 
-@plug.handler
+@plug.handler()
 def read_chunk(filename, offset, size):
     return ''
 
-@plug.handler
+@plug.handler()
 def write_chunk(filename, offset, chunk):
     return -1
 
-@plug.handler
+@plug.handler()
 def new_file(metadatas):
     pass
 
@@ -28,11 +28,12 @@ def start(*args, **kwargs):
     plug.launch(*args, **kwargs)
     port = int(plug.options['port'])
     serv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    serv.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     serv.bind(('', port))
-    print "Starting client's driver on port {}".format(port)
     serv.listen(1)
+    print "Starting client's driver on port {}".format(port)
     plug.client, _ = serv.accept()
     client_thread = Thread(None, client_handler, 'client')
     client_thread.start()
-    print('finished')
+    print(plug.handlers)
     plug.join()
