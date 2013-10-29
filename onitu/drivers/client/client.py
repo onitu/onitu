97911ffd870ff12f4874d59e5d75176ddc549283
Antoine Rozo <antoine.rozo@gmail.com>
2013-10-29 17:41:29 +0100

    Commands sent to client

--- onitu/drivers/client/client.py
+++ onitu/drivers/client/client.py
@@ -1,28 +1,49 @@
 from onitu.api.plug import Plug
 import socket
 from threading import Thread
+import struct
+
+protocol = {
+    'RC': '256sll',
+    'WF': '256sll',
+    'NF': '256s256s'
+}
+
+class Client:
+    def __init__(self, socket=None):
+        self.socket = socket
+        self.commands = []
 
 plug = Plug()
-plug.client = None
+plug.client = Client()
+
+def send_cmd(opcode, *args):
+    fmt = '2s' + protocol[opcode]
+    s = struct.pack(fmt, opcode, *args)
+    plug.client.socket.send(s)
+    print(s)
+    print(struct.unpack(fmt, s))
 
 @plug.handler()
 def read_chunk(filename, offset, size):
+    send_cmd('RC', filename, offset, size)
+    #return plug.client.commands.pop()
     return ''
 
 @plug.handler()
 def write_chunk(filename, offset, chunk):
-    return -1
+    send_cmd('WC', filename, offset, size)
 
 @plug.handler()
 def new_file(metadatas):
-    pass
+    send_cmd('NF', metadatas['path'], metadatas['filename'])
 
 def client_handler():
     while True:
-        data = plug.client.recv(1024)
+        data = plug.client.socket.recv(1024)
         if not data:
             break
-        print(data)
+        plug.client.commands.append(data)
 
 def start(*args, **kwargs):
     plug.launch(*args, **kwargs)
@@ -32,8 +53,8 @@ def start(*args, **kwargs):
     serv.bind(('', port))
     serv.listen(1)
     print "Starting client's driver on port {}".format(port)
-    plug.client, _ = serv.accept()
+    plug.client.socket, _ = serv.accept()
     client_thread = Thread(None, client_handler, 'client')
     client_thread.start()
-    print(plug.handlers)
+    print(plug.handlers['read_chunk']('tutu', 0, 512))
     plug.join()
