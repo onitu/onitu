df503d673a21e206a0ad1501570eb20f483487f5
Antoine Rozo <antoine.rozo@gmail.com>
2013-11-03 19:36:52 +0100

    Notifier on client Few bugs: - doesn't truc file when the new file has a size lesser than the old one           - Double-write the file on updates

--- onitu/drivers/client/client.py
+++ onitu/drivers/client/client.py
@@ -12,27 +12,30 @@ plug.req = context.socket(zmq.REQ)
 @plug.handler()
 def read_chunk(filename, offset, size):
     plug.req.send_multipart(('read_chunk', filename, str(offset), str(size)))
-    return plug.req.recv_multipart()
+    resp = plug.req.recv_multipart()
+    if resp[0] != 'ok':
+        raise FileNotFoundError(filename)
+    return resp[1]
 
 @plug.handler()
 def write_chunk(filename, offset, chunk):
     plug.req.send_multipart(('write_chunk', filename, str(offset), chunk))
-    print plug.req.recv_multipart()
+    plug.req.recv_multipart()
 
 def rep_handler():
     while True:
         msg = plug.rep.recv_multipart()
         if msg[0] == 'connect':
             plug.rep.send_multipart(('accept', plug.options['port2']))
+        elif msg[0] == 'file_updated':
+            _, filename, size, last_update = msg
+            metadata = plug.get_metadata(filename)
+            metadata.size = int(size)
+            metadata.last_update = float(last_update)
+            plug.update_file(metadata)
+            plug.rep.send_multipart(('ok',))
         else:
-            plug.rep.send_multipart(('cmd not found', msg[0]))
-        print msg
-
-from time import sleep
-def test_handler():
-    while True:
-        print plug.handlers['read_chunk']('tutu', 0, 1024)
-        sleep(2)
+            plug.rep.send_multipart(('ko', 'cmd not found', msg[0]))
 
 def start(*args, **kwargs):
     plug.launch(*args, **kwargs)
@@ -42,6 +45,4 @@ def start(*args, **kwargs):
     print "Starting client's driver on port {}".format(port)
     rep_thread = Thread(None, rep_handler, 'rep')
     rep_thread.start()
-    test_thread = Thread(None, test_handler, 'test')
-    test_thread.start()
     plug.join()
