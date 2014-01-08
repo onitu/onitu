8734fbbd7c3f3238138cc15cf9bc3ba6f96e15bc
Antoine Rozo <antoine.rozo@gmail.com>
2014-01-08 14:58:43 +0100

    Generate files and calculate md5sum

---
+++ tests/functional/utils/files.py
@@ -0,0 +1,12 @@
+import sh
+import hashlib
+
+def generate(filename, size):
+    return sh.dd('if=/dev/urandom', 'of={}'.format(filename), 'bs={}'.format(size), 'count=1')
+
+def checksum(filename):
+    h = hashlib.md5()
+    with open(filename, 'rb') as f:
+        for chunk in iter(lambda: f.read(8192), b''):
+            h.update(chunk)
+    return h.hexdigest()
