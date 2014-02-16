f1669e0d00533013d72f979819ef0c2ec955b4d3
Antoine Rozo <antoine.rozo@gmail.com>
2014-02-16 17:34:32 +0100

    Delete files after test in test_changes

--- tests/functional/test_changes.py
+++ tests/functional/test_changes.py
@@ -39,26 +39,33 @@ def launcher_startup(launcher):
     loop.run(timeout=5)
 
 
-def launch_with_files(launcher, reps, prefix, n, size):
+def launch_with_files(launcher, reps, prefix, n, size, delete=True):
     launcher.unset_all_events()
-
     files = ['{}{}'.format(prefix, i) for i in range(n)]
 
-    for filename in files:
-        generate(os.path.join(reps['rep1'], filename), size)
+    try:
+        for filename in files:
+            generate(os.path.join(reps['rep1'], filename), size)
 
-    loop = CounterLoop(n)
-    for filename in files:
-        launcher.on_transfer_ended(
-            loop.check, d_from='rep1', d_to='rep2', filename=filename
-        )
+        loop = CounterLoop(n)
+        for filename in files:
+            launcher.on_transfer_ended(
+                loop.check, d_from='rep1', d_to='rep2', filename=filename
+            )
 
-    launcher_startup(launcher)
-    loop.run(timeout=(5 + n // 10))
-    for filename in files:
-        assert(checksum(os.path.join(reps['rep1'], filename)) ==
-               checksum(os.path.join(reps['rep2'], filename)))
-    launcher.kill()
+        launcher_startup(launcher)
+        loop.run(timeout=(5 + n // 10))
+        for filename in files:
+            assert(checksum(os.path.join(reps['rep1'], filename)) ==
+                   checksum(os.path.join(reps['rep2'], filename)))
+    finally:
+        launcher.kill()
+        if delete:
+            for filename in files:
+                try:
+                    unlink(filename)
+                except:
+                    pass
 
 
 def test_changes_one_file():
@@ -74,5 +81,5 @@ def test_changes_many_files():
 
 
 def test_updating_files():
-    launch_with_files(launchers[1], reps[1], 'up', 10, 100)
+    launch_with_files(launchers[1], reps[1], 'up', 10, 100, False)
     launch_with_files(launchers[1], reps[1], 'up', 10, 100)
