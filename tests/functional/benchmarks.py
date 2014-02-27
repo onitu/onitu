2c80961862b5caba7f059b3d54ba05f42fb07282
Louis Roché <louis@louisroche.net>
2014-02-27 22:50:09 +0800

    Benchmarks: enable BenchmarkMultipleCopy

--- tests/functional/benchmarks.py
+++ tests/functional/benchmarks.py
@@ -56,117 +56,131 @@ class BenchmarkSimpleCopy(Benchmark):
 
     def test_small(self):
         total = BenchmarkData('test_small', 'Copy 1000 times a 1M file')
-        for i in range(10):
+        for i in range(1000):
             try:
                 t = self.copy_file('small', SMALL)
                 total.add_result(t)
             except BaseException as e:
+                self._log('Error in test_small')
                 self._log(e)
         return total
 
-#    def test_medium(self):
-#        total = BenchmarkData('Copy 100 times a 10M file')
-#        for i in range(100):
-#            total.add_result(self.copy_file('medium', MEDIUM))
-#        return total
-#
-#    def test_big(self):
-#        total = BenchmarkData('Copy 10 times a 100M file')
-#        for i in range(10):
-#            total.add_result(self.copy_file('big', BIG))
-#        return total
+    def test_medium(self):
+        total = BenchmarkData('test_medium', 'Copy 100 times a 10M file')
+        for i in range(100):
+            try:
+                t = self.copy_file('medium', MEDIUM)
+                total.add_result(t)
+            except BaseException as e:
+                self._log('Error in test_medium')
+                self._log(e)
+        return total
+
+    def test_big(self):
+        total = BenchmarkData('test_big', 'Copy 10 times a 100M file')
+        for i in range(10):
+            try:
+                t = self.copy_file('big', BIG)
+                total.add_result(t)
+            except BaseException as e:
+                self._log('Error in test_big')
+                self._log(e)
+        return total
 
 
-#class BenchmarkMultipleCopy(Benchmark):
-#    def launch_onitu(self):
-#        self.launcher = None
-#        self.json_file = 'bench_multiple_copy.json'
-#        self.rep1 = LocalStorageDriver('rep1')
-#        self.rep2 = LocalStorageDriver('rep2')
-#        self.rep3 = LocalStorageDriver('rep3')
-#        setup = Setup()
-#        setup.add(*self.rep1.setup)
-#        setup.add(*self.rep2.setup)
-#        setup.add(*self.rep3.setup)
-#        setup.save(self.json_file)
-#        loop = CounterLoop(4)
-#        self.launcher = Launcher(self.json_file)
-#        self.launcher.on_referee_started(loop.check)
-#        self.launcher.on_driver_started(loop.check, driver='rep1')
-#        self.launcher.on_driver_started(loop.check, driver='rep2')
-#        self.launcher.on_driver_started(loop.check, driver='rep3')
-#        self.launcher()
-#        loop.run(timeout=5)
-#
-#    def stop_onitu(self):
-#        self.launcher.kill()
-#        unlink(self.json_file)
-#        self.rep1.close()
-#        self.rep2.close()
-#        self.rep3.close()
-#
-#    def setup(self):
-#        self.launch_onitu()
-#
-#    def teardown(self):
-#        self.stop_onitu()
-#
-#    def copy_file(self, filename, size, timeout=20):
-#        self.launcher.unset_all_events()
-#        loop = BooleanLoop()
-#        loop = CounterLoop(2)
-#        self.launcher.on_transfer_ended(
-#            loop.check, d_from='rep1', d_to='rep2', filename=filename
-#        )
-#        self.launcher.on_transfer_ended(
-#            loop.check, d_from='rep1', d_to='rep3', filename=filename
-#        )
-#        self.rep1.generate(filename, size)
-#        with Timer() as t:
-#            loop.run(timeout=timeout)
-#        assert self.rep1.checksum(filename) == self.rep2.checksum(filename)
-#        assert self.rep1.checksum(filename) == self.rep3.checksum(filename)
-#        return t.msecs
-#
-#    def test_small(self):
-#        total = {
-#            'description': 'Copy 1000 times a 1M file',
-#            'unit': 'ms',
-#            'results': {}
-#        }
-#        for i in range(1000):
-#            total['results'][i] = self.copy_file('small', SMALL)
-#        return total
-#
-#    def test_medium(self):
-#        total = {
-#            'description': 'Copy 100 times a 10M file',
-#            'unit': 'ms',
-#            'results': {}
-#        }
-#        for i in range(100):
-#            total['results'][i] = self.copy_file('medium', MEDIUM)
-#        return total
-#
-#    def test_big(self):
-#        total = {
-#            'description': 'Copy 10 times a 100M file',
-#            'unit': 'ms',
-#            'results': {}
-#        }
-#        for i in range(10):
-#            total['results'][i] = self.copy_file('BIG', BIG)
-#        return total
+class BenchmarkMultipleCopy(Benchmark):
+    def launch_onitu(self):
+        self.launcher = None
+        self.json_file = 'bench_multiple_copy.json'
+        self.rep1 = LocalStorageDriver('rep1')
+        self.rep2 = LocalStorageDriver('rep2')
+        self.rep3 = LocalStorageDriver('rep3')
+        setup = Setup()
+        setup.add(*self.rep1.setup)
+        setup.add(*self.rep2.setup)
+        setup.add(*self.rep3.setup)
+        setup.save(self.json_file)
+        loop = CounterLoop(4)
+        self.launcher = Launcher(self.json_file)
+        self.launcher.on_referee_started(loop.check)
+        self.launcher.on_driver_started(loop.check, driver='rep1')
+        self.launcher.on_driver_started(loop.check, driver='rep2')
+        self.launcher.on_driver_started(loop.check, driver='rep3')
+        self.launcher()
+        loop.run(timeout=5)
+
+    def stop_onitu(self):
+        self.launcher.kill()
+        unlink(self.json_file)
+        self.rep1.close()
+        self.rep2.close()
+        self.rep3.close()
+
+    def setup(self):
+        self.launch_onitu()
+
+    def teardown(self):
+        self.stop_onitu()
+
+    def copy_file(self, filename, size, timeout=20):
+        self.launcher.unset_all_events()
+        loop = BooleanLoop()
+        loop = CounterLoop(2)
+        self.launcher.on_transfer_ended(
+            loop.check, d_from='rep1', d_to='rep2', filename=filename
+        )
+        self.launcher.on_transfer_ended(
+            loop.check, d_from='rep1', d_to='rep3', filename=filename
+        )
+        self.rep1.generate(filename, size)
+        with Timer() as t:
+            loop.run(timeout=timeout)
+        assert self.rep1.checksum(filename) == self.rep2.checksum(filename)
+        assert self.rep1.checksum(filename) == self.rep3.checksum(filename)
+        return t.msecs
+
+    def test_small(self):
+        total = BenchmarkData('test_small', 'Copy 1000 times a 1M file')
+        for i in range(1000):
+            try:
+                t = self.copy_file('small', SMALL)
+                total.add_result(t)
+            except BaseException as e:
+                self._log('Error in test_small')
+                self._log(e)
+        return total
+
+    def test_medium(self):
+        total = BenchmarkData('test_medium', 'Copy 100 times a 10M file')
+        for i in range(100):
+            try:
+                t = self.copy_file('medium', MEDIUM)
+                total.add_result(t)
+            except BaseException as e:
+                self._log('Error in test_medium')
+                self._log(e)
+        return total
+
+    def test_big(self):
+        total = BenchmarkData('test_big', 'Copy 10 times a 100M file')
+        for i in range(10):
+            try:
+                t = self.copy_file('big', BIG)
+                total.add_result(t)
+            except BaseException as e:
+                self._log('Error in test_big')
+                self._log(e)
+        return total
 
 if __name__ == '__main__':
     bench_simple = BenchmarkSimpleCopy(verbose=True)
     bench_simple.run()
-    #bench_multiple = BenchmarkMultipleCopy(verbose=True)
-    #bench_multiple.run()
+    bench_multiple = BenchmarkMultipleCopy(verbose=True)
+    bench_multiple.run()
     print('{:=^28}'.format(' simple copy '))
     bench_simple.display()
-    #print('{:=^28}'.format(' multiple copy '))
-    #bench_multiple.display()
+    print('{:=^28}'.format(' multiple copy '))
+    bench_multiple.display()
     # TODO: clean this shit
     if len(argv) >= 7 and argv[1] in ('-u', '--upload'):
         host = argv[2]
