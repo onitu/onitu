63495b53e2c6c47be89f134a8bc071abc6e9637c
Yannick PEROUX <yannick.peroux@gmail.com>
2014-03-07 19:44:09 +0000

    Add possibility to use custom options in tests' drivers

--- tests/utils/driver.py
+++ tests/utils/driver.py
@@ -7,38 +7,51 @@ from .tempdirs import dirs
 
 
 class Driver(object):
-    def __init__(self, name):
+    def __init__(self, name, chunk_size=None):
+        self.type = None
+        self.root = None
         self.name = name
+        self.chunk_size = chunk_size
 
+    def options(self):
+        options = {
+            'root': self.root
+        }
 
-class LocalStorageDriver(Driver):
-    def __init__(self, name):
-        super(LocalStorageDriver, self).__init__(name)
-        self.directory = dirs.create()
+        if self.chunk_size:
+            options['chunk_size'] = self.chunk_size
+
+        return options
 
     @property
     def setup(self):
-        return ('local_storage', self.name, {'root': self.directory})
+        return (self.type, self.name, self.options())
+
+
+class LocalStorageDriver(Driver):
+    def __init__(self, *args, **kwargs):
+        super(LocalStorageDriver, self).__init__(*args, **kwargs)
+        self.type = 'local_storage'
+        self.root = dirs.create()
 
     def close(self):
-        dirs.delete(self.directory)
+        dirs.delete(self.root)
 
     def mkdir(self, subdirs):
-        return sh.mkdir('-p', os.path.join(self.directory, subdirs))
+        return sh.mkdir('-p', os.path.join(self.root, subdirs))
 
     def write(self, filename, content):
-        with open(os.path.join(self.directory, filename), 'w+') as f:
+        with open(os.path.join(self.root, filename), 'w+') as f:
             f.write(content)
 
     def generate(self, filename, size):
-        return files.generate(os.path.join(self.directory, filename),
-                              size)
+        return files.generate(os.path.join(self.root, filename), size)
 
     def unlink(self, filename):
-        return os.unlink(os.path.join(self.directory, filename))
+        return os.unlink(os.path.join(self.root, filename))
 
     def checksum(self, filename):
-        return files.checksum(os.path.join(self.directory, filename))
+        return files.checksum(os.path.join(self.root, filename))
 
 
 class TargetDriver(Driver):
@@ -46,10 +59,10 @@ class TargetDriver(Driver):
         'local_storage': LocalStorageDriver
     }
 
-    def __new__(cls, name):
+    def __new__(cls, *args, **kwargs):
         driver = os.environ.get('ONITU_TEST_DRIVER', 'local_storage')
         try:
             driver_type = cls._types[driver]
         except KeyError:
             raise KeyError("No such driver {}".format(repr(driver)))
-        return driver_type(name)
+        return driver_type(*args, **kwargs)
