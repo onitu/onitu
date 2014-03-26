4dd321d6d06c584f02d27026371c207a4f751a34
Antoine Rozo <antoine.rozo@gmail.com>
2014-03-26 19:33:11 +0100

    Test rules with mimetypes

--- tests/functional/test_rules.py
+++ tests/functional/test_rules.py
@@ -1,5 +1,7 @@
 from os import unlink
 
+import pytest
+
 from tests.utils.launcher import Launcher
 from tests.utils.setup import Setup, Rule
 from tests.utils.driver import LocalStorageDriver, TargetDriver
@@ -28,7 +30,7 @@ def launcher_startup():
     launcher.on_driver_started(loop.check, driver='rep1')
     launcher.on_driver_started(loop.check, driver='rep2')
     launcher()
-    loop.run(timeout=5)
+    loop.run(timeout=10)
     launcher.unset_all_events()
 
 
@@ -60,7 +62,7 @@ def test_no_rule():
         launcher.kill()
 
 
-def test_simple_path():
+def test_path():
     try:
         directory = 'foo'
         filename = '{}/bar'.format(directory)
@@ -79,5 +81,113 @@ def test_simple_path():
         rep1.generate(filename, 100)
         loop.run(timeout=5)
         assert rep1.checksum(filename) == rep2.checksum(filename)
+    finally:
+        launcher.kill()
+
+
+def test_not_mime():
+    try:
+        filename = 'bar.txt'
+        setup = Setup(session=True)
+        setup.add(rep1)
+        setup.add(rep2)
+        setup.add_rule(Rule().match_mime('image/png').sync('rep2'))
+        setup.save(json_file)
+        launcher_startup()
+
+        try:
+            loop = BooleanLoop()
+            launcher.on_transfer_started(
+                loop.stop, d_from='rep1', d_to='rep2', filename=filename
+            )
+            rep1.generate(filename, 100)
+            loop.run(timeout=1)
+        except TimeoutError:
+            pass
+        else:
+            raise ShouldNotCopy
+    finally:
+        launcher.kill()
+
+
+def test_simple_mime():
+    try:
+        filename = 'bar.png'
+        setup = Setup(session=True)
+        setup.add(rep1)
+        setup.add(rep2)
+        setup.add_rule(Rule().match_mime('image/png').sync('rep2'))
+        setup.save(json_file)
+        launcher_startup()
+
+        loop = BooleanLoop()
+        launcher.on_transfer_ended(
+            loop.stop, d_from='rep1', d_to='rep2', filename=filename
+        )
+        rep1.generate(filename, 100)
+        loop.run(timeout=5)
+        assert rep1.checksum(filename) == rep2.checksum(filename)
+    finally:
+        launcher.kill()
+
+
+def test_multi_mime():
+    try:
+        filenames = 'bar.png', 'foo.txt'
+        setup = Setup(session=True)
+        setup.add(rep1)
+        setup.add(rep2)
+        setup.add_rule(Rule().match_mime('image/png', 'text/plain').sync('rep2'))
+        setup.save(json_file)
+        launcher_startup()
+
+        for filename in filenames:
+            loop = BooleanLoop()
+            launcher.on_transfer_ended(
+                loop.stop, d_from='rep1', d_to='rep2', filename=filename
+            )
+            rep1.generate(filename, 100)
+            loop.run(timeout=5)
+            assert rep1.checksum(filename) == rep2.checksum(filename)
+    finally:
+        launcher.kill()
+
+
+@pytest.mark.xfail
+def test_path_mime():
+    try:
+        directory = 'foo'
+        setup = Setup(session=True)
+        setup.add(rep1)
+        setup.add(rep2)
+        setup.add_rule(Rule().match_path('/{}'.format(directory))
+                       .match_mime('image/png').sync('rep2'))
+        setup.save(json_file)
+        launcher_startup()
+
+        rep1.mkdir(directory)
+
+        filename = 'foo/bar.png'
+        loop = BooleanLoop()
+        launcher.on_transfer_ended(
+            loop.stop, d_from='rep1', d_to='rep2', filename=filename
+        )
+        rep1.generate(filename, 100)
+        loop.run(timeout=5)
+        assert rep1.checksum(filename) == rep2.checksum(filename)
+
+        filenames = 'bar.png', 'foo/bar'
+        for filename in filenames:
+            try:
+                loop = BooleanLoop()
+                launcher.on_transfer_started(
+                    loop.stop, d_from='rep1', d_to='rep2', filename=filename
+                )
+                rep1.generate(filename, 100)
+                loop.run(timeout=1)
+            except TimeoutError:
+                pass
+            else:
+                raise ShouldNotCopy
     finally:
         launcher.kill()
