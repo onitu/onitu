6ec555dbb48e68a4e31542f1bc9ae5935a136d92
Alexandre Baron <alexandre.baron@epitech.eu>
2014-05-14 16:44:20 +0200

    * Rollback to the real Proof of Concept original version instead of the broken multipart one

--- onitu/drivers/amazon_s3/amazon_s3.py
+++ onitu/drivers/amazon_s3/amazon_s3.py
@@ -8,12 +8,12 @@ plug = Plug()
 
 S3Conn = None
 
-multipart_upload = None
-part_num = 0
+# Not implemented yet
+#multipart_upload = None
+#part_num = 0
 
 TIMESTAMP_FMT = '%a, %d %b %Y %H:%M:%S GMT' # example: 'Sat, 03 May 2014 04:36:11 GMT'
-ALL_KEYS_TIMESTAMP_FMT = '%Y-%m-%dT%H:%M:%S.000Z' # using the get_all_keys method, format changes!
-# '%Y-%m-%dT%H:%M:%S %Z'
+ALL_KEYS_TIMESTAMP_FMT = '%Y-%m-%dT%H:%M:%S.000Z' # using the get_all_keys method, format changes
 
 
 def get_bucket():
@@ -41,7 +41,8 @@ def get_chunk(metadata, offset, size):
 
 @plug.handler()
 def start_upload(metadata):
-    global part_num
+#    global part_num
+#    global multipart_upload
 
     bucket = get_bucket()
     if bucket:
@@ -51,14 +52,28 @@ def start_upload(metadata):
             key.key = metadata.filename
             key.set_contents_from_string("")
         # Creating a new S3 multipart upload
-        print 'New multipart upload for', metadata.filename
-        multipart_upload = bucket.initiate_multipart_upload(metadata.filename, headers={'Content-Type': 'application/octet-stream'})
-        part_num = 0
+        # (not implemented yet)
+        # multipart_upload = bucket.initiate_multipart_upload(metadata.filename, headers={'Content-Type': 'application/octet-stream'})
+        # # metadata.revision currently is a string holding two informations: timestamp of the file's last revision and multipart upload ID, separated by a '\t'.
+        # # Here, as we start a multipart upload we update the id. Useful to resume the upload in case of a crash.
+        # try:
+        #     info_list = metadata.revision.split('\t')
+        # except AttributeError: # no revision data
+        #     info_list = ['', '']
+        # try:
+        #     info_list[1] = str(multipart_upload.id)
+        # except IndexError: # No multipart upload id stored right now
+        #     print 'index error'
+        #     info_list.append(str(multipart_upload.id))
+        # except Exception as e:
+        #     print e
+        # metadata.write_revision('\t'.join(info_list))
+        # part_num = 0
 
 @plug.handler()
 def end_upload(metadata):
-    global part_num
-
+#    global part_num
+#    global multipart_upload
     bucket = get_bucket()
     if bucket:
         key = bucket.get_key(metadata.filename) # find the key on S3
@@ -68,18 +83,14 @@ def end_upload(metadata):
             new_revision = time.strptime(key.last_modified, TIMESTAMP_FMT) # convert timestamp to timestruct
             new_revision = time.mktime(metadata.revision) # convert timestruct to float
             new_revision = str(new_revision) # convert float to string
-            print 'New timestamp:', new_revision
-            revision_update = metadata.revision.split('\t')
-            revision_update[0] = new_revision
-            print 'new revision:', '\t'.join(revision_update)
-            metadata.write_revision('\t'.join(revision_update))
-        multipart_upload.complete_upload()
-        print 'end mp upload'
-        part_num = 0
+            metadata.write_revision(new_revision)
+            # multipart_upload.complete_upload()
+
 
 @plug.handler()
 def upload_chunk(metadata, offset, chunk):
-    global part_num
+#    global part_num
+#    global multipart_upload
 
     bucket = get_bucket()
     if bucket:
@@ -87,21 +98,24 @@ def upload_chunk(metadata, offset, chunk):
         if not key:
             plug.logger.error("Upload chunk: " + metadata.filename + ": No such file")
         else:
-            print 'Uploading chunk', part_num
-            upload_fp = StringIO.StringIO() # Amazon S3 multipart upload only takes its part content from a file. Emulating it with StringIO
-            upload_fp.write(chunk)
-            multipart_upload.upload_part_from_file(fp=upload_fp, part_num=part_num)
-            upload_fp.close()
-            part_num += 1
-            # contents = list(key.get_contents_as_string())
-            # chunk_size = len(chunk)-1
-            # contents[offset:chunk_size] = list(chunk)
-            # key.set_contents_from_string(''.join(contents))
+            contents = list(key.get_contents_as_string())
+            chunk_size = len(chunk)-1
+            contents[offset:chunk_size] = list(chunk)
+            key.set_contents_from_string(''.join(contents))
+            # Multipart upload (not implemented yet)
+            # upload_fp = StringIO.StringIO() # Amazon S3 multipart upload only takes its part content from a file. Emulating it with StringIO
+            # upload_fp.write(chunk)
+            # multipart_upload.upload_part_from_file(fp=upload_fp, part_num=part_num)
+            # upload_fp.close()
+            # part_num += 1
 
 @plug.handler()
 def abort_upload(metadata):
-    if multipart_upload:
-         multipart_upload.cancel_upload()
+    pass
+    # Not Implemented yet
+    # global multipart_upload
+    # if multipart_upload:
+    #      multipart_upload.cancel_upload()
 
 class CheckChanges(threading.Thread):
 
@@ -116,17 +130,16 @@ class CheckChanges(threading.Thread):
             keys = bucket.get_all_keys()
             for key in keys:
                 metadata = plug.get_metadata(key.name)
-                onitu_revision = float(metadata.revision.split('\t')[0])
+                try:
+                    onitu_revision = float(metadata.revision)
+                except (ValueError, TypeError): # float cast failed or metadata.revision is None, possibly because that's a new file
+                    onitu_revision = 0.0
                 remote_revision = time.mktime(time.strptime(key.last_modified, ALL_KEYS_TIMESTAMP_FMT))
-                # Some evil timestamp black magic is happening when making times !
-                # Sometimes even if metadata.revision is clearly inferior to the remote_revision, it evaluates to False.
-                # Explicitly casting to float works around the problem (although they already are floats ...)
-                if onitu_revision < remote_revision: 
+                if onitu_revision < remote_revision:
                     print 'update:', key.name, '!'
                     metadata.size = key.size
                     metadata.revision = remote_revision
                     plug.update_file(metadata)
-                    
 
     def run(self):
         while not self.stop.isSet():
@@ -138,9 +151,9 @@ class CheckChanges(threading.Thread):
 
 
 def start(*args, **kwargs):
-    plug.initialize(*args, **kwargs)
     global S3Conn
 
+    plug.initialize(*args, **kwargs)
     error_msg = ""
     changes_timer = 0
     try: # S3 Connection
