7bfd71f65f3fe3a0fca1f244fbea704cd3813ad2
Louis Roché <louis@louisroche.net>
2014-05-18 01:02:38 -0700

    Fix the multiples copies

--- tests/benchmarks/benchmarks.py
+++ tests/benchmarks/benchmarks.py
@@ -146,31 +146,13 @@ class BenchmarkSimpleCopy(Benchmark):
 
 class BenchmarkMultipleCopies(Benchmark):
     def launch_onitu(self):
-        self.launcher = None
-        self.json_file = 'bench_multiple_copy.json'
-        self.rep1 = LocalStorageDriver('rep1')
-        self.rep2 = LocalStorageDriver('rep2')
-        self.rep3 = LocalStorageDriver('rep3')
-        setup = Setup()
-        setup.add(self.rep1)
-        setup.add(self.rep2)
-        setup.add(self.rep3)
-        setup.save(self.json_file)
-        loop = CounterLoop(4)
-        self.launcher = Launcher(self.json_file)
-        self.launcher.on_referee_started(loop.check)
-        self.launcher.on_driver_started(loop.check, driver='rep1')
-        self.launcher.on_driver_started(loop.check, driver='rep2')
-        self.launcher.on_driver_started(loop.check, driver='rep3')
-        self.launcher()
-        loop.run(timeout=5)
+        launcher(self, 3)
 
     def stop_onitu(self):
         self.launcher.kill()
         unlink(self.json_file)
-        self.rep1.close()
-        self.rep2.close()
-        self.rep3.close()
+        for rep in self.reps:
+            rep.close()
 
     def setup(self):
         self.launch_onitu()
@@ -180,7 +162,6 @@ class BenchmarkMultipleCopies(Benchmark):
 
     def copy_file(self, filename, size, timeout=20):
         self.launcher.unset_all_events()
-        loop = BooleanLoop()
         loop = CounterLoop(2)
         self.launcher.on_transfer_ended(
             loop.check, d_from='rep1', d_to='rep2', filename=filename
@@ -188,18 +169,22 @@ class BenchmarkMultipleCopies(Benchmark):
         self.launcher.on_transfer_ended(
             loop.check, d_from='rep1', d_to='rep3', filename=filename
         )
-        self.rep1.generate(filename, size)
+        self.reps[0].generate(filename, size)
         with Timer() as t:
             loop.run(timeout=timeout)
-        assert self.rep1.checksum(filename) == self.rep2.checksum(filename)
-        assert self.rep1.checksum(filename) == self.rep3.checksum(filename)
+        assert (
+            self.reps[0].checksum(filename) == self.reps[1].checksum(filename)
+        )
+        assert (
+            self.reps[0].checksum(filename) == self.reps[2].checksum(filename)
+        )
         return t.msecs
 
     def test_small(self):
         total = BenchmarkData('test_small', 'Copy 1000 times a 1M file')
         for i in range(1000):
             try:
-                t = self.copy_file('small', SMALL)
+                t = self.copy_file('small{}'.format(i), SMALL)
                 total.add_result(t)
             except BaseException as e:
                 self.log.warn('Error in test_small')
@@ -210,7 +195,7 @@ class BenchmarkMultipleCopies(Benchmark):
         total = BenchmarkData('test_medium', 'Copy 100 times a 10M file')
         for i in range(100):
             try:
-                t = self.copy_file('medium', MEDIUM)
+                t = self.copy_file('medium{}'.format(i), MEDIUM)
                 total.add_result(t)
             except BaseException as e:
                 self.log.warn('Error in test_medium')
@@ -221,7 +206,7 @@ class BenchmarkMultipleCopies(Benchmark):
         total = BenchmarkData('test_big', 'Copy 10 times a 100M file')
         for i in range(10):
             try:
-                t = self.copy_file('big', BIG)
+                t = self.copy_file('big{}'.format(i), BIG)
                 total.add_result(t)
             except BaseException as e:
                 self.log.warn('Error in test_big')
@@ -231,12 +216,12 @@ class BenchmarkMultipleCopies(Benchmark):
 if __name__ == '__main__':
     bench_simple = BenchmarkSimpleCopy('BENCH_SIMPLE_COPY', verbose=True)
     bench_simple.run()
-    # bench_multiple = BenchmarkMultipleCopies(
-    #     'Bench multiple copies',
-    #     verbose=True
-    # )
-    # bench_multiple.run()
+    bench_multiple = BenchmarkMultipleCopies(
+        'BENCH_MULTIPLE_COPIES',
+        verbose=True
+    )
+    bench_multiple.run()
     print('{:=^28}'.format(' simple copy '))
     bench_simple.display()
-    # print('{:=^28}'.format(' multiple copy '))
-    # bench_multiple.display()
+    print('{:=^28}'.format(' multiple copies '))
+    bench_multiple.display()
