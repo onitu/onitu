aef5d85abcf1b7833d3de4ef26913a9aaa46a483
Yannick PEROUX <yannick.peroux@gmail.com>
2014-05-23 22:11:20 +0100

    Update Metadata in order to use Escalator
    - New 'extra' argument for entry's attributes
    - Remove 'revision' attribute
    - Update the doc
    - Initialize all the properties in __init__
    - Use the new fids
    - Use a hash for the properties

--- onitu/api/metadata.py
+++ onitu/api/metadata.py
@@ -1,3 +1,6 @@
+from onitu.utils import get_fid
+
+
 class Metadata(object):
     """The Metadata class represent the metadata of any file in Onitu.
 
@@ -14,121 +17,72 @@ class Metadata(object):
         The absolute filename of the file
     **size**
         The size of the file, in octets
-    **revision**
-        This field is specific to each entry. It is a string
-        representing the current revision of the file for the
-        current entry.
-        The drivers should compare an upstream and a local version
-        of a file with this field. The format is dependant from the
-        driver (it can be whatever you want: a timestamp, a number,
-        a hash...).
     **owners**
         The entries which should have this file
     **uptodate**
         The entries with an up-to-date version of this file
+
+    Each entry can also store extra informations via the :attr:`.extra`
+    attribute. It's a dictionary which can contain any kind of information,
+    as long as it's JSON serializable.
+    Those informations will not be shared with the other entries, as they
+    are stocked separately.
     """
 
-    PROPERTIES = {
-        'filename': (str, str),
-        'size': (int, str),
-        'owners': (lambda e: e.split(':'), lambda l: ':'.join(l)),
-        'uptodate': (lambda e: e.split(':'), lambda l: ':'.join(l)),
-    }
+    PROPERTIES = ('filename', 'size', 'owners', 'uptodate')
 
-    def __init__(self, plug=None, filename=None, size=0):
+    def __init__(self, plug=None, filename=None, size=0,
+                 fid=None, owners=[], uptodate=[]):
         super(Metadata, self).__init__()
 
         self.filename = filename
         self.size = size
-        self.owners = []
-        self.uptodate = []
+        self.owners = owners
+        self.uptodate = uptodate
+
+        if not fid and filename:
+            self.fid = get_fid(filename)
+        elif fid:
+            self.fid = fid
+
+        self.extra = {}
 
         self.plug = plug
-        self.session = plug.session
-
-        self._revision = None
-        self._fid = None
 
     @classmethod
     def get_by_filename(cls, plug, filename):
         """Instantiate a new :class:`.Metadata` object for the file
         with the given name.
         """
-        fid = plug.session.hget('files', filename)
-
-        if fid:
-            return cls.get_by_id(plug, fid)
-        else:
-            return None
+        fid = get_fid(filename)
+        return cls.get_by_id(plug, fid)
 
     @classmethod
     def get_by_id(cls, plug, fid):
         """Instantiate a new :class:`.Metadata` object for the file
         with the given id.
         """
-        values = plug.session.hgetall('files:{}'.format(fid))
+        values = plug.escalator.get('file:{}'.format(fid), default=None)
 
         if not values:
             return None
 
-        metadata = cls(plug)
-        metadata._fid = fid
+        metadata = cls(plug, fid=fid, **values)
 
-        for name, (deserialize, _) in cls.PROPERTIES.items():
-            metadata.__setattr__(name, deserialize(values.get(name)))
+        metadata.extra = plug.escalator.get(
+            'file:{}:entry:{}'.format(fid, plug.name),
+            default={}
+        )
 
         return metadata
 
-    @property
-    def revision(self):
-        """The revision of a file can be any string, and should be
-        used to compare the different versions of a file on driver.
-
-        Each driver can use its own system of revisions, as it is
-        stored for each entry.
-        """
-        if self._revision:
-            return self._revision
-        elif self._fid:
-            return self.session.hget(
-                'drivers:{}:files'.format(self.plug.name),
-                self._fid
-            )
-
-    @revision.setter
-    def revision(self, value):
-        self._revision = value
-
-    def write_revision(self):
-        """Write the current revision in the database. Called
-        by :meth:`.write`.
-        """
-        if not self._revision:
-            return
-
-        self.session.hset(
-            'drivers:{}:files'.format(self.plug.name),
-            self._fid,
-            self._revision
-        )
-
-        self._revision = None
-
     def write(self):
-        """Write the metadata of the current file in the database.
-        """
-        metadata = {}
+        """Write the metadata of the current file in the database."""
 
-        for name, (_, serialize) in self.PROPERTIES.items():
-            try:
-                metadata[name] = serialize(self.__getattribute__(name))
-            except AttributeError:
-                self.plug.logger.error(
-                    "Error writing metadata for {}, missing attribute {}",
-                    self._fid, name
-                )
-                return
+        values = dict((p, self.__getattribute__(p)) for p in self.PROPERTIES)
 
-        self.session.hmset('files:{}'.format(self._fid), metadata)
-
-        self.write_revision()
+        with self.plug.escalator.write_batch() as batch:
+            batch.put('file:{}'.format(self.fid), values)
+            batch.put(
+                'file:{}:entry:{}'.format(self.fid, self.plug.name), self.extra
+            )
