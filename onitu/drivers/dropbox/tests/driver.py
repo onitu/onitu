8715324d1c466d617fb832841413025b464b7012
Louis Roché <louis@louisroche.net>
2014-07-04 10:31:37 -0700

    Fix the imports in the tests of dropbox

--- onitu/drivers/dropbox/tests/driver.py
+++ onitu/drivers/dropbox/tests/driver.py
@@ -4,7 +4,7 @@ from path import path
 
 from tests.utils.testdriver import TestDriver
 from tests.utils.tempdirs import dirs
-from onitu.drivers.dropbox.libDropbox import LibDropbox
+from onitu.drivers.dropbox.dropboxDriver import dropboxDriver
 
 class Driver(TestDriver):
     def __init__(self, *args, **options):
@@ -16,7 +16,7 @@ class Driver(TestDriver):
             options['secret'] = "g4favy0bgjstt2w"
         if 'changes_timer' not in options:
             options['changes_timer'] = 600.0
-        self.google_drive = LibDrive(options)
+        self.dropbox = dropboxDriver(options)
         super(Driver, self).__init__('dropbox',
                                      *args,
                                      **options)
