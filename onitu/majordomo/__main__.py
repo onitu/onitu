d1f65723790fc68bb7ecbe6afddc20f11c0f81d3
Antoine Rozo <antoine.rozo@gmail.com>
2014-08-04 11:40:42 +0200

    Majorodomo for managing remote drivers (clients)

---
+++ onitu/majordomo/__main__.py
@@ -0,0 +1,87 @@
+import sys
+import uuid
+
+#import zmq.auth
+#from zmq.auth.thread import ThreadAuthenticator
+from circus.client import CircusClient
+
+from . import broker
+from onitu.escalator.client import Escalator
+from logbook.queues import ZeroMQHandler
+from logbook import Logger
+
+log_uri = sys.argv[1]
+escalator_uri = sys.argv[2]
+session = sys.argv[3]
+escalator = Escalator(escalator_uri, session)
+
+
+class Broker(broker.Broker):
+    def __init__(self):
+        super(Broker, self).__init__((20001, 20003))
+        self.circus_client = CircusClient()
+        self.nb_remotes = 0
+
+    def _handle_socket(self, socket):
+        if socket is not self.frontend.reqs:
+            return super(Broker, self)._handle_socket(socket)
+        from_id, to_id, msg = self._get_msg(socket)
+        if not to_id and msg == b'start':
+            to_id = uuid.uuid4().hex
+            self.nb_remotes += 1
+            name = 'remote-{}'.format(self.nb_remotes)
+            escalator.put('entry:{}:driver'.format(name), 'remote_driver')
+            escalator.put('entry:{}:options'.format(name), {
+                'id': to_id,
+                'remote_id': from_id,
+                'remote_uri': 'tcp://127.0.0.1:{}'.format(
+                    self.backend.reps_port),
+                'handlers_uri': 'tcp://127.0.0.1:{}'.format(
+                    self.backend.reqs_port)
+            })
+            # protect query over non-ascii identities
+            query = {
+                "command": 'add',
+                "properties": {
+                    "name": name,
+                    "cmd": sys.executable,
+                    "args": ('-m',
+                             'onitu.drivers',
+                             'remote_driver',
+                             escalator_uri,
+                             session,
+                             name,
+                             log_uri),
+                    "copy_env": True,
+                    "start": True
+                }
+            }
+            self.circus_client.call(query)
+            socket.send_multipart((from_id, b'', to_id))
+            return
+        self._handle_relay(socket, from_id, to_id, msg)
+
+
+if __name__ == '__main__':
+    with ZeroMQHandler(log_uri, multi=True).applicationbound():
+        #ctx = zmq.Context.instance()
+        #auth = ThreadAuthenticator(ctx)
+        #auth.start()
+        #auth.configure_curve(domain='*', location='authorized_keys')
+        #clients_req = ctx.socket(zmq.ROUTER)
+        #clients_req.curve_server = True
+        #clients_req.curve_publickey, clients_req.curve_secretkey \
+        #    = zmq.auth.load_certificate('server.key_secret')
+        #server_req = ctx.socket(zmq.DEALER)
+        #clients_req.bind('tcp://*:55443')
+        #server_req.bind('tcp://*:55444')
+
+        logger = Logger('Majordomo')
+        logger.info('Started')
+
+        broker = Broker()
+        while True:
+            broker.poll()
+
+        #auth.stop()
+        logger.info('Exited')
---
+++ onitu/majordomo/broker.py
@@ -0,0 +1,78 @@
+import zmq
+
+
+class Broker(object):
+
+    class Interface(object):
+        def __init__(self, protocol='tcp', addr='*', ports=(None, None)):
+            ctx = zmq.Context.instance()
+            self.reqs = ctx.socket(zmq.ROUTER)
+            self.reps = ctx.socket(zmq.ROUTER)
+            self.reqs_port = self._bind_socket(self.reqs,
+                                               protocol,
+                                               addr,
+                                               ports[0])
+            self.reps_port = self._bind_socket(self.reps,
+                                               protocol,
+                                               addr,
+                                               ports[1])
+
+        @staticmethod
+        def _bind_socket(socket, protocol, addr, port):
+            if port is None:
+                port = socket.bind_to_random_port('{}://{}'.format(protocol,
+                                                                   addr))
+            else:
+                socket.bind('{}://{}:{}'.format(protocol, addr, port))
+            return port
+
+    def __init__(self, frontend_ports, backend_ports=(None, None),
+                 frontend_addr='*', backend_addr='*',
+                 frontend_protocol='tcp', backend_protocol='tcp'):
+        self.frontend = self.Interface(frontend_protocol,
+                                       frontend_addr,
+                                       frontend_ports)
+        self.backend = self.Interface(backend_protocol,
+                                      backend_addr,
+                                      backend_ports)
+
+        self.poller = zmq.Poller()
+        self.poller.register(self.frontend.reqs, zmq.POLLIN)
+        self.poller.register(self.frontend.reps, zmq.POLLIN)
+        self.poller.register(self.backend.reqs, zmq.POLLIN)
+        self.poller.register(self.backend.reps, zmq.POLLIN)
+
+        self.relays = {
+            self.frontend.reqs: self.backend.reps,
+            self.backend.reqs: self.frontend.reps,
+            self.frontend.reps: self.backend.reqs,
+            self.backend.reps: self.frontend.reqs
+        }
+        self.logs = {
+            self.frontend.reqs: 'F-REQ',
+            self.backend.reqs: 'B-REQ',
+            self.frontend.reps: 'F-REP',
+            self.backend.reps: 'B-REP'
+        }
+
+    def _get_msg(self, socket):
+        print self.logs.get(socket, 'ERROR')
+        from_id, _, to_id, msg = socket.recv_multipart()
+        print '--', from_id, to_id, msg
+        return from_id, to_id, msg
+
+    def _handle_socket(self, socket):
+        from_id, to_id, msg = self._get_msg(socket)
+        if not to_id and msg == b'ready':
+            return
+        self._handle_relay(socket, from_id, to_id, msg)
+
+    def _handle_relay(self, socket, from_id, to_id, msg):
+        to = self.relays.get(socket, None)
+        if to:
+            to.send_multipart((to_id, b'', from_id, msg))
+
+    def poll(self):
+        for socket, t in self.poller.poll():
+            if t == zmq.POLLIN:
+                self._handle_socket(socket)
