62b3e54d8a06437de49376711e9eaa6038b84a3b
Louis Roché <louis@louisroche.net>
2014-09-14 15:36:37 -0700

    Fix a compat error between py27 and py33

--- onitu/api/__main__.py
+++ onitu/api/__main__.py
@@ -92,7 +92,7 @@ def timeout():
 @app.route('/api/v1.0/files', method='GET')
 def get_files():
     files = [metadata for key, metadata in escalator.range('file:')
-             if key.count(':') == 1]
+             if key.count(b':') == 1]
     for metadata in files:
         metadata['fid'] = get_fid(metadata['filename'])
     return {'files': files}
