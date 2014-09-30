e28c105ce133b835580fea37f7006bcf8e6b3a88
Maxime Constantinian <maxime.constantinian@epitech.eu>
2014-10-01 01:49:40 +0200

    fix problem with test. Still need too check if all test pass.

--- onitu/drivers/google_drive/tests/driver.py
+++ onitu/drivers/google_drive/tests/driver.py
@@ -12,26 +12,10 @@ import onitu.drivers.google_drive.libdrive as libdrive
 refresh_token = "1/ezUs-qa0qMRXYDj4x0rcq0ODO_1nG-qiG-3POqzjs8w"
 
 
-def get_token(client_id, client_secret):
-    global access_token
-    global token_expi
-    if time.time() + 20.0 < token_expi:
-        return
-    ret_val, _, data = libdrive.get_token("6155769202.apps.googleusercontent.com",
-                                          "ZcxluuTcGL2WkurnYSJgJvbN",
-                                          refresh_token)
-    data = json.loads(data)
-    if ret_val == 200:
-        access_token = data["access_token"]
-        token_expi = time.time() + data["expires_in"]
-    else:
-        plug.logger.error("Can not get token: " + data["error"]["message"])
-
-
 class Driver(TestDriver):
     def __init__(self, *args, **options):
         if 'root' not in options:
-            options['root'] = dirs.create()
+            options['root'] = "onitu-test"
         if 'refresh_token' not in options:
             options['refresh_token'] = refresh_token
         if 'client_id' not in options:
@@ -40,87 +24,89 @@ class Driver(TestDriver):
             options['client_secret'] = "ZcxluuTcGL2WkurnYSJgJvbN"
         if 'changes_timer' not in options:
             options['changes_timer'] = 2.0
-        global root_id
-        root_id = "root"
-        global token_expi
-        token_expi = 0
-        global access_token
-        access_token = ""
-        get_token(options['client_id'], options['client_secret'])
+        self.root_id = "root"
+        self.token_expi = 0
+        self.access_token = ""
+        self.get_token(options['client_id'], options['client_secret'])
 	self.options = options
 
         path = options["root"].split("/")
 	path = [p for p in path if p != u""]
 
         for f in path:
-            ret_val, _, data = libdrive.get_information(access_token, f, root_id)
+            ret_val, _, data = libdrive.get_information(self.access_token, f, self.root_id)
             data = json.loads(data)
             if ret_val == 200:
                 if (data["items"] != []):
-                    root_id = data["items"][0]["id"]
+                    self.root_id = data["items"][0]["id"]
                 else:
-                    ret_val, _, data = libdrive.add_folder(access_token, f, root_id)
+                    ret_val, _, data = libdrive.add_folder(self.access_token, f, self.root_id)
 		    data = json.loads(data)
                     if ret_val == 200:
-                        root_id = data["id"]
+                        self.root_id = data["id"]
 
         super(Driver, self).__init__('google_drive',
                                      *args,
                                      **options)
+
+    def get_token(self, client_id, client_secret):
+        if time.time() + 20.0 < self.token_expi:
+            return
+        ret_val, _, data = libdrive.get_token("6155769202.apps.googleusercontent.com",
+                                              "ZcxluuTcGL2WkurnYSJgJvbN",
+                                              refresh_token)
+        data = json.loads(data)
+        if ret_val == 200:
+            self.access_token = data["access_token"]
+            self.token_expi = time.time() + data["expires_in"]
 
     @property
     def root(self):
         return path(self.options['root'])
 
     def close(self):
-        get_token(self.options['client_id'], self.options['client_secret'])
-	global root_id
-	global access_token
-        libdrive.delete_by_id(access_token, root_id)
+        self.get_token(self.options['client_id'], self.options['client_secret'])
+        libdrive.delete_by_id(self.access_token, self.root_id)
         #self.google_drive.delete_id(self.google_drive.folder_root_id)
 
     def mkdir(self, subdirs):
-        get_token(self.options['client_id'], self.options['client_secret'])
-	global root_id
-	global access_token
+        self.get_token(self.options['client_id'], self.options['client_secret'])
         path = subdirs.split("/")
 	path = [p for p in path if p != u""]
-        tmproot = root_id
+        tmproot = self.root_id
         for f in path:
-            ret_val, _, data = libdrive.get_information(access_token, f, tmproot)
+            ret_val, _, data = libdrive.get_information(self.access_token, f, tmproot)
             data = json.loads(data)
             if ret_val == 200:
                 if (data["items"] != []):
                     tmproot = data["items"][0]["id"]
                 else:
-                    ret_val, _, data = libdrive.add_folder(access_token, f, tmproot)
+                    ret_val, _, data = libdrive.add_folder(self.access_token, f, tmproot)
                     if ret_val == 200:
                         data = json.loads(data)
                         tmproot = data["id"]
     # self.google_drive.add_folders(subdirs+"/toto", False)
 
     def write(self, filename, content):
-        get_token(self.options['client_id'], self.options['client_secret'])
-	global root_id
-	global access_token
+        self.get_token(self.options['client_id'], self.options['client_secret'])
         path = filename.split("/")
 	path = [p for p in path if p != u""]
-        tmproot = root_id
+        tmproot = self.root_id
         if len (path) > 1:
             for f in path[:len(path)-1]:
-                ret_val, _, data = libdrive.get_information(access_token, f, tmproot)
+                ret_val, _, data = libdrive.get_information(self.access_token, f, tmproot)
                 data = json.loads(data)
                 if ret_val == 200:
                     if (data["items"] != []):
                         tmproot = data["items"][0]["id"]
                     else:
-                        ret_val, _, data = libdrive.add_folder(access_token, f, tmproot)
+                        ret_val, _, data = libdrive.add_folder(self.access_token, f, tmproot)
                         if ret_val == 200:
                             data = json.loads(data)
                             tmproot = data["id"]
         self_id = None
-        ret_val, h, data = libdrive.start_upload(access_token, path[len(path)-1], tmproot, self_id)
-        ret_val, _, data = libdrive.upload_chunk(access_token, h["location"],
+        ret_val, h, data = libdrive.start_upload(self.access_token, path[len(path)-1], tmproot, self_id)
+        ret_val, _, data = libdrive.upload_chunk(self.access_token, h["location"],
                                                  0, content, len(content))
         
         # self.google_drive.start_upload(filename, len(content))
@@ -131,66 +117,60 @@ class Driver(TestDriver):
         self.write(filename, os.urandom(size))
 
     def unlink(self, filename):
-        get_token(self.options['client_id'], self.options['client_secret'])
-	global root_id
-	global access_token
+        self.get_token(self.options['client_id'], self.options['client_secret'])
         path = filename.split("/")
 	path = [p for p in path if p != u""]
-        tmproot = root_id
+        tmproot = self.root_id
         for f in path[:len(path)-1]:
-            ret_val, _, data = libdrive.get_information(access_token, f, tmproot)
+            ret_val, _, data = libdrive.get_information(self.access_token, f, tmproot)
             data = json.loads(data)
             if ret_val == 200:
                 if (data["items"] != []):
                     tmproot = data["items"][0]["id"]
                 else:
-                    ret_val, _, data = libdrive.add_folder(access_token, f, tmproot)
+                    ret_val, _, data = libdrive.add_folder(self.access_token, f, tmproot)
                     if ret_val == 200:
                         data = json.loads(data)
                         tmproot = data["id"]
         id_folder = tmproot
-        ret_val, _, info = libdrive.get_information(access_token, path[len(path)-1], id_folder)
+        ret_val, _, info = libdrive.get_information(self.access_token, path[len(path)-1], id_folder)
         info = json.loads(info)
-        libdrive.delete_by_id(access_token, info["items"][0]["id"])
+        libdrive.delete_by_id(self.access_token, info["items"][0]["id"])
 
     def checksum(self, filename):
-        get_token(self.options['client_id'], self.options['client_secret'])
-	global root_id
-	global access_token
+        self.get_token(self.options['client_id'], self.options['client_secret'])
         path = filename.split("/")
 	path = [p for p in path if p != u""]
-	tmproot = root_id
+	tmproot = self.root_id
         for f in path[:len(path)-1]:
-            ret_val, _, data = libdrive.get_information(access_token, f, tmproot)
+            ret_val, _, data = libdrive.get_information(self.access_token, f, tmproot)
             data = json.loads(data)
             if ret_val == 200:
                 if (data["items"] != []):
                     tmproot = data["items"][0]["id"]
         id_folder = tmproot
-        ret_val, _, info = libdrive.get_information(access_token, path[len(path)-1], id_folder)
+        ret_val, _, info = libdrive.get_information(self.access_token, path[len(path)-1], id_folder)
         info = json.loads(info)
         return info["items"][0]["md5Checksum"]
 
     def exists(self, filename):
-        get_token(self.options['client_id'], self.options['client_secret'])
-	global root_id
-	global access_token
-        tmproot = root_id
+        self.get_token(self.options['client_id'], self.options['client_secret'])
+        tmproot = self.root_id
         path = filename.split("/")
 	path = [p for p in path if p != u""]
         for f in path[:len(path)-1]:
-            ret_val, _, data = libdrive.get_information(access_token, f, tmproot)
+            ret_val, _, data = libdrive.get_information(self.access_token, f, tmproot)
             data = json.loads(data)
             if ret_val == 200:
                 if (data["items"] != []):
                     tmproot = data["items"][0]["id"]
                 else:
-                    ret_val, _, data = libdrive.add_folder(access_token, f, tmproot)
+                    ret_val, _, data = libdrive.add_folder(self.access_token, f, tmproot)
                     if ret_val == 200:
                         data = json.loads(data)
                         tmproot = data["id"]
         id_folder = tmproot
-        ret_val, _, info = libdrive.get_information(access_token, path[len(path)-1], id_folder)
+        ret_val, _, info = libdrive.get_information(self.access_token, path[len(path)-1], id_folder)
         info = json.loads(info)
         if info["items"] != []:
             return True
