3d5acb76e2f0e4b73597086030061e73f4b5156e
Antoine Rozo <antoine.rozo@gmail.com>
2014-10-09 09:13:41 +0200

    Adapt remote-driver to new architecture

---
+++ drivers/remote_driver/setup.py
@@ -0,0 +1,21 @@
+import sys
+from setuptools import setup, find_packages
+
+setup(
+    name="onitu-remote-driver",
+    version="0.1",
+    url="http://onitu.github.io",
+    description="Launch a driver on a remote computer",
+    license="MIT",
+    packages=find_packages(),
+    install_requires=[],
+    package_data={'': ['manifest.json']},
+    entry_points={
+        'onitu.drivers': [
+            'remote_driver = onitu_remote_driver'
+        ],
+        'onitu.tests': [
+            'remote_driver = onitu_remote_driver.tests'
+        ]
+    }
+)
