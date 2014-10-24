8a4860dbe6b292306b4a162b2357428a7f625493
Yannick PÉROUX <yannick.peroux@gmail.com>
2014-10-24 19:33:35 +0200

    Delete __main__.py

--- drivers/google_drive/onitu_google_drive/__main__.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-from logbook.queues import ZeroMQHandler
-
-from .google_drive import start
-
-if __name__ == '__main__':
-    with ZeroMQHandler(sys.argv[2], multi=True).applicationbound():
-        start(sys.argv[1])
