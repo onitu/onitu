2daf5cd9cf0fde9e8f3267d9b19f22635475239f
Akiat <akiat.bordel@gmail.com>
2014-12-27 01:10:16 +0100

    Add the new working C# version

---
+++ onitu/gui/windows-extension/MyIconOverlayHandlers/MyIconOverlayHandlersAdded/MyIconOverlayHandler.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using Microsoft.Win32;
+using System.IO;
+using MyIconOverlayHandlers;
+ 
+namespace MyIconOverlayHandlersAdded
+{
+    [ComVisible(true)]
+    [Guid("9854f366-eb51-4189-84d0-1833400a6a09")]
+    public class AMyIconOverlayHandlersAdded : MyIconOverlayHandlerBase
+    {
+        #region Class Properties
+ 
+        protected override string OverlayIconFilePath
+        {
+            get
+            {
+                return Path.Combine(base.OverlayIconFilePath, @"AddedOverlay.ico");
+            }
+        }
+ 
+        protected override string FileNameStart
+        {
+            get
+            {
+                return "added";
+            }
+        }
+ 
+        #endregion Class Properties
+    }
+}
---
+++ onitu/gui/windows-extension/MyIconOverlayHandlers/MyIconOverlayHandlersPending/MyIconOverlayHandlers.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using Microsoft.Win32;
+using System.IO;
+using MyIconOverlayHandlers;
+
+namespace MyIconOverlayHandlersPending
+{
+    [ComVisible(true)]
+    [Guid("4e4b5be0-8a16-4768-83af-2bf0c139cfbc")]
+    public class AMyIconOverlayHandlersPending : MyIconOverlayHandlerBase
+    {
+        #region Class Properties
+
+        protected override string OverlayIconFilePath
+        {
+            get
+            {
+                return Path.Combine(base.OverlayIconFilePath, @"PendingOverlay.ico");
+            }
+        }
+
+        protected override string FileNameStart
+        {
+            get
+            {
+                return "pending";
+            }
+        }
+
+        #endregion Class Properties
+    }
+}
