f39179b71fb045316c65480fec1e986b6bb1b9eb
Louis Roché <louis@louisroche.net>
2015-01-17 00:25:25 +0100

    Detect the deletions on webdav's side

--- drivers/wd/onitu_webdav/wd.py
+++ drivers/wd/onitu_webdav/wd.py
@@ -22,7 +22,6 @@ def create_dirs(path):
     dirs = path.split('/')
     for d in dirs[1:]:
         p = '/'.join([root, d])
-        plug.logger.debug(p)
         webd.mkdir(p)
         root = p
 
@@ -30,7 +29,7 @@ def create_dirs(path):
 @plug.handler()
 def delete_file(metadata):
     plug.logger.debug("delete file: {}", metadata.path)
-    webd.unpublish(metadata.path)
+    webd.clean(metadata.path)
 
 
 @plug.handler()
@@ -108,47 +107,75 @@ class CheckChanges(threading.Thread):
         if path.endswith('/'):
             path = path[:-1]
         plug.logger.debug("check folder: path {}", path)
+
         try:
             filelist = self.webdav.list(path)
         except Exception as e:
             raise ServiceError(
                 "Error listing file in '{}': {}".format(path, e)
             )
-
         plug.logger.debug("filelist {}", filelist)
+
+        # list all the files of a folder
+        files = []
+
         for f in filelist:
             filepath = u('/'.join([path, f]))
-
-            if self.webdav.is_dir(filepath):
-                self.check_folder(filepath)
-            else:
-                infos = self.webdav.info(filepath)
-                plug.logger.debug('infos: {}', infos)
-                metadata = plug.get_metadata(filepath)
-                if metadata is None:
-                    plug.logger.debug("ignore file {}", filepath)
-                    continue
-                mtime_onitu = metadata.extra.get(
-                    'revision',
-                    None
-                )
-                if mtime_onitu is None:
-                    mtime_onitu = datetime.datetime.min
+            try:
+                if self.webdav.is_dir(filepath):
+                    sub_files = self.check_folder(filepath)
+                    for sub_file in sub_files:
+                        files.append(sub_file)
                 else:
-                    mtime_onitu = datetime.datetime.strptime(
-                        mtime_onitu, TIMESTAMP_FMT
+                    files.append(filepath)
+                    infos = self.webdav.info(filepath)
+                    plug.logger.debug('infos {}: {}', filepath, infos)
+                    metadata = plug.get_metadata(filepath)
+                    if metadata is None:
+                        plug.logger.debug("ignore file {}", filepath)
+                        continue
+                    mtime_onitu = metadata.extra.get(
+                        'revision',
+                        None
                     )
-                mtime_local = datetime.datetime.strptime(
-                    infos['modified'], TIMESTAMP_FMT
+                    if mtime_onitu is None:
+                        mtime_onitu = datetime.datetime.min
+                    else:
+                        mtime_onitu = datetime.datetime.strptime(
+                            mtime_onitu, TIMESTAMP_FMT
+                        )
+                    mtime_local = datetime.datetime.strptime(
+                        infos['modified'], TIMESTAMP_FMT
+                    )
+                    if mtime_local > mtime_onitu:
+                        update_file(metadata, f, infos)
+            except wc.WebDavException as exception:
+                plug.logger.debug(
+                    ("Exception while the informations about a file "
+                     "were fetched: {}"), exception
                 )
-                if mtime_local > mtime_onitu:
-                    update_file(metadata, f, infos)
+                files.append(filepath)
+                continue
+        return files
+
+    def delete_files(self, folder, files):
+        old_files = plug.list(folder).keys()
+        plug.logger.debug("old_files {}", old_files)
+        for f in old_files:
+            filepath = '/'.join([folder.path, f])
+            if filepath in events_to_ignore:
+                continue
+            if filepath not in files:
+                metadata = plug.get_metadata(filepath)
+                plug.delete_file(metadata)
 
     def run(self):
         while not self.stop.isSet():
             try:
                 for folder in plug.folders_to_watch:
-                    self.check_folder(folder.path)
+                    files = self.check_folder(folder.path)
+                    plug.logger.debug("list of files {}", files)
+                    self.delete_files(folder, files)
             except EscalatorClosed:
                 # We are closing
                 return
