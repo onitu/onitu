016c129e6bd466c59c652cc946fdd9f579e90bf6
Yannick PEROUX <yannick.peroux@gmail.com>
2015-01-30 19:05:45 +0100

    API: Use the new uptodate system for get_file

--- onitu/api/__main__.py
+++ onitu/api/__main__.py
@@ -190,7 +190,10 @@ def get_file_content(fid):
     metadata = escalator.get('file:{}'.format(fid), default=None)
     if not metadata:
         return file_not_found(fid)
-    service = metadata['uptodate'][0]
+    services = escalator.range(
+        u'file:{}:uptodate:'.format(fid), include_value=False
+    )
+    service = services[0].split(':')[-1]
     content = call_handler(service, FILE, fid)
     if content[0] != ERROR:
         response.content_type = metadata['mimetype']
